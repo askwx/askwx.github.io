16a80456db69604b75f6ac7b55e3362402b41057
Asuka <asuka@tlu.ee>
2021-02-22 16:55:17 +0200

    First version

---
+++ script.js
@@ -0,0 +1,23 @@
+const navbar = document.querySelector(".navbar");
+const navbarOffsetTop = navbar.offsetTop;
+const sections = document.querySelectorAll('section');
+const navbarLinks = document.querySelectorAll('.navbar-link');
+const progress = document.querySelector('.progress-bars-wrapper');
+const progressBarPercents = [100,100,100,100,100];
+
+window.addEventListener('scroll',() =>{
+    if (window.pageYOffset >= navbarOffsetTop){
+        navbar.classList.add('sticky')
+    }else{
+        navbar.classList.remove('sticky');
+    }
+
+    sections.forEach((section, i ) => {
+        if(window.pageYOffset >= section. offsetTop -10){
+            navbarLinks.forEach(navbarLink =>{
+                navbarLink.classList.remove("change")
+            })
+            navbarLinks[i].classList.add("change");
+        }
+    });
+});
